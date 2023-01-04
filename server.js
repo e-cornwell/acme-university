e1967cde18207bc4b77ea66a01cb3632c8410e2a
Ethan Cornwell <ethan.aubrey22@gmail.com>
2023-01-04 09:46:00 -0700

    added web pages and got port working again

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=> {
-    res.write('hello world');
-    res.end();
+    if(req.url === '/foo'){
+        res.write('tbd foo page');
+        res.end();
+    }
+    else if (req.url === '/bar'){
+        res.write('bar page');
+        res.end();
+    }
+    else if (req.url === '/bazz'){
+        res.write('bazz page');
+        res.end();
+    }
+    else{
+        res.statusCode = 404;
+        res.write('This Page Doesnt Exist');
+        res.write('This page doesnt exist!');
+        res.end();
+    }
+    
+
 });
 
 const port = process.env.PORT || 3000;
